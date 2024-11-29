07fc58a0e58598b1d28c44c68ceb804127c8584e
HCook <166083664+Hayden-Cook@users.noreply.github.com>
2024-11-29 14:33:46 -0600

    moved App.css to styles and have continued to make CSS edits

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
+import './styles/App.css';
 
 const App = () => {
   return (
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/AboutMe.css';
 
+
 // AboutMe component. This will show the user a picture and a short bio about myself.
 const AboutMe = () => {
     return (
         <section>
             <h2>About Me</h2>
-            <img src="" alt="Hayden Cook's picture" />
+            <img src="../public/ProfilePic.png" alt="Hayden Cook's picture" />
             <p>Insert a short bio about yourself...</p>
         </section>
     );
